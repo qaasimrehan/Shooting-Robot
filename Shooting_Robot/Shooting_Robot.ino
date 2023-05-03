f2d5a5b077968b6992f86c6675931a1ea8737088
Qaasim Rehan <qaasimrehan@gmail.com>
2023-05-02 22:38:55 -0400

    added arcade drive

--- Shooting_Robot/Shooting_Robot.ino
+++ Shooting_Robot/Shooting_Robot.ino
@@ -38,10 +38,13 @@ enum PS4Variable {
   NumPS4Variables
 };
 
+//Deadzone
+const int deadzone = 20;  // Anything between -20 and 20 is stop
+
 void setup() {
   //set up I2C
   Wire.begin();
-  
+
   // Start serial communication at 115200 baud
   Serial.begin(115200);
 
@@ -68,35 +71,82 @@ void Receive() {
     }
 
     // Print to the serial monitor for testing purposes
-   /* Serial.print("Message: ");
-    for (int i = 0; i < 30; i++) {
-      Serial.print(PS4Controller[i]);
-      Serial.print(", ");
-    }
-    Serial.println();*/
+    /* Serial.print("Message: ");
+      for (int i = 0; i < 30; i++) {
+       Serial.print(PS4Controller[i]);
+       Serial.print(", ");
+      }
+      Serial.println();*/
   }
 }
 
-void Drive(){
-  if (PS4Controller[LStickY] > 20){
-  leftJrk.setTarget(2348);
-  rightJrk.setTarget(2348);
-  }  else if (PS4Controller[LStickY] < -20){
-  leftJrk.setTarget(1748);
-  rightJrk.setTarget(1748);
+void Driveold() {
+  double angle = map(PS4Controller[RStickX], 128, -128, -10000.0, 10000.0);
+  double linear = map(PS4Controller[LStickY], -128, 128, -4000.0, 4000.0);
+
+  if (PS4Controller[LStickY] < 15 && PS4Controller[LStickY] > -15) {
+    linear = 0;
   }
-  if (PS4Controller[LStickY] > -10 && PS4Controller[LStickY] < 10){
-    leftJrk.stopMotor();
-    rightJrk.stopMotor();
+  if (PS4Controller[RStickX] < 15 && PS4Controller[RStickX] > -15) {
+    angle = 0;
   }
 
-  
-  Serial.println(PS4Controller[LStickY]);
+
+  double speed_wish_right = (angle * 0.3) / 2 + linear;
+  double speed_wish_left = linear * 2 - speed_wish_right;
+
+  int leftTarget = map(speed_wish_left, -3500, 3500, 1448, 2648);
+  int rightTarget = map(speed_wish_right, -3500, 3500, 1448, 2648);
+
+  Serial.print (speed_wish_left);
+  Serial.print(" ");
+  Serial.print (speed_wish_right);
+  Serial.println();
+
+  leftJrk.setTarget(leftTarget);
+  rightJrk.setTarget(rightTarget);
+
 }
+int pulseToPWM(int pulse) {
 
+ 
+    pulse = map(pulse, -128, 128, -500, 500);
+    pulse = constrain(pulse, -255, 255);
+ 
+
+  // Anything in deadzone should stop the motor  
+  if ( abs(pulse) <= deadzone ) {
+    pulse = 0;
+  }
+
+  return pulse;
+}
+void Drive(int speed_a, int speed_b) {
+
+speed_a = map(speed_a, -255, 255, 1448, 2648);
+speed_b = map(speed_b, -255, 255, 1448, 2648);
+
+  // Limit speed between 1448 and 2648
+  speed_a = constrain(speed_a, 1448, 2648);
+  speed_b = constrain(speed_b, 1448, 2648);
+
+  // Set speed
+  leftJrk.setTarget(speed_a);
+  rightJrk.setTarget(speed_b);
+}
 
 void loop() {
-Drive();
-Receive();
-//Serial.println(PS4Controller[RStickX]);
+  Receive();
+int y = pulseToPWM(PS4Controller[LStickY]);
+int x = pulseToPWM(PS4Controller[RStickX]);
+    // Mix for arcade drive
+  int left = y + x;
+  int right = y - x;
+Serial.print (left);
+  Serial.print(" ");
+  Serial.print (right);
+  Serial.println();
+  // Drive motor
+  Drive(left, right);
+
 }
